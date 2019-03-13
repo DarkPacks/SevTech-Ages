DarkPacks/SevTech-Ages

commit 96bfbe0fd795eef4b9c1f4334a236a91296d18fd

fix: hide ImmersiveTech/Petroleum multiblocks in JEI (#3611)
* fix: hide ImmersiveTech/Petroleum multiblocks in JEI
Closes #3610

* fix: correct naming

src/scripts/crafttweaker/staging/itemsAndRecipes/mods/immersivepetroleum.zs
@@ -1,0 +1,12 @@
+import crafttweaker.item.IIngredient;
+
+import scripts.crafttweaker.classes.utils.recipeUtil;
+
+static hidden as IIngredient[] = [
+	<immersivepetroleum:metal_multiblock>,
+	<immersivepetroleum:dummy>
+];
+
+function init() {
+	recipeUtil.hideItems(hidden as IIngredient[], false);
+}

src/scripts/crafttweaker/postInit.zs
@@ -115,6 +115,7 @@ function initItemsAndRecipesStaging() {
 	scripts.crafttweaker.staging.itemsAndRecipes.mods.huntingdim.init();
 	scripts.crafttweaker.staging.itemsAndRecipes.mods.immcraft.init();
 	scripts.crafttweaker.staging.itemsAndRecipes.mods.immersiveengineering.init();
+	scripts.crafttweaker.staging.itemsAndRecipes.mods.immersivepetroleum.init();
 	scripts.crafttweaker.staging.itemsAndRecipes.mods.immersivetech.init();
 	scripts.crafttweaker.staging.itemsAndRecipes.mods.improvedbackpacks.init();
 	scripts.crafttweaker.staging.itemsAndRecipes.mods.indlog.init();

src/scripts/crafttweaker/staging/itemsAndRecipes/mods/immersivetech.zs
@@ -10,8 +10,15 @@ static stagedItems as IIngredient[][string] = {
 	]
 };
 
+static hiddenRemove as IIngredient[] = [
+	<immersivetech:stone_multiblock>,
+	<immersivetech:metal_multiblock>
+];
+
 function init() {
 	for stageName, items in stagedItems {
 		ZenStager.getStage(stageName).addIngredients(items);
 	}
+
+	recipeUtil.hideItems(hiddenRemove as IIngredient[], true);
 }

commit 5cfafb4faf2d628e14b84d95e8ecaa127bfbc600

fix: error introduced in #3611
recipeUtil is a global!

src/scripts/crafttweaker/staging/itemsAndRecipes/mods/immersivepetroleum.zs
@@ -1,7 +1,5 @@
 import crafttweaker.item.IIngredient;
 
-import scripts.crafttweaker.classes.utils.recipeUtil;
-
 static hidden as IIngredient[] = [
 	<immersivepetroleum:metal_multiblock>,
 	<immersivepetroleum:dummy>
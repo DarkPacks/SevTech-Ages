d5cb20778aeca56e38d04be079f05d595f30739e
Justin Russo <j.russo@outlook.com>
2020-06-14 02:53:08 -0400

    build: implement external mod downloading

---
+++ download-external-mods.bat
@@ -0,0 +1,1 @@
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0\pack-tools\scripts\dl-external-mods.ps1""'}"

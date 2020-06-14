d8ffdd7c23dffc0ff7078c73189173daf45bb1c6
Justin Russo <j.russo@outlook.com>
2020-06-14 05:00:02 -0400

    build: set up instancesync

---
+++ setup.bat
@@ -0,0 +1,1 @@
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0\pack-tools\scripts\setup.ps1""'}"
